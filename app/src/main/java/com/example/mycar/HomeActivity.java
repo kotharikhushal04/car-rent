b168e8eb0cfff7b280ba17abd7513b9108d20d29
khushalkothari <khushal.kothari04@gmail.com>
2022-01-28 13:01:49 +0530

    first_carproject

--- app/src/main/java/com/example/mycar/HomeActivity.java
+++ app/src/main/java/com/example/mycar/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.mycar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,4 +13,32 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+    public void logoclick(View view){
+        Intent intent = new Intent(getApplicationContext(),myaccount.class);
+        startActivity(intent);
+    }
+
+    public void mercrdesclick(View view){
+        Intent intent = new Intent(getApplicationContext(),mercedescol.class);
+        startActivity(intent);
+    }
+
+    public void bmwclick(View view){
+        Intent intent = new Intent(getApplicationContext(),bmwcol.class);
+        startActivity(intent);
+    }
+
+    public void hundaiclick(View view){
+        Intent intent = new Intent(getApplicationContext(),hundaicol.class);
+        startActivity(intent);
+
+    }
+
+    public void volvoclick(View view){
+        Intent intent = new Intent(getApplicationContext(),volvocol.class);
+        startActivity(intent);
+
+    }
+
+
 }
